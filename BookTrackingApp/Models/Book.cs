71cd2a2a32cf8d8dc5a318c12a6baaecc1fd4ae1
hafees <hafees.falana@gmail.com>
2021-12-17 21:14:09 +0100

    Data annotations added in models

--- BookTrackingApp/Models/Book.cs
+++ BookTrackingApp/Models/Book.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookTrackingApp.Models
 {
     public class Book
-    {
+    {   
+        [Key]
+        public string ISBN { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
