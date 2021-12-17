95f39da56868b3d2dc02a5e85c5a205eb26d6704
hafees <hafees.falana@gmail.com>
2021-12-17 22:44:14 +0100

    caching added to start up file

--- BookTrackingApp/Startup.cs
+++ BookTrackingApp/Startup.cs
@@ -41,6 +41,9 @@ namespace BookTrackingApp
                     ResponseCompressionDefaults.MimeTypes.Concat(
                         new[] { "image/svg+xml" });
             });
+
+            //adding cache
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
